b8afe4cdb907d1911b5b2f2cf9b8e75cd1ebcbb7
Nindyaull <pindangoreng123@gmail.com>
2025-10-03 21:11:42 +0700

    modifikasi user 1

--- Test.java
+++ Test.java
@@ -3,5 +3,8 @@ public class Test {
         System.out.println("Hi, ini baris kode pertama"); 
         System.out.println("Halo, ini baris kode kedua");
         System.out.println("Hola, ini baris kode tiga");
+
+        System.out.println("Hai, ini baris kode pertama yang dimodifikasi user 1");
+        System.out.println("User 1 menambah baris baru");
     } 
 }
